e9244b888ac375415c1311e2724076ed77b5b0db
kaznak <kaznak.at.work@gmail.com>
2019-07-25 15:43:13 +0900

    add aws-amplify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import Amplify from 'aws-amplify'
 
 Vue.config.productionTip = false
 
