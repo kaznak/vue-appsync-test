dca0e0cbbedc2f33bb5bc22d754fc5a57d656759
kaznak <kaznak.at.work@gmail.com>
2019-07-25 15:46:49 +0900

    add AWS AppSync access credential placeholder.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import Amplify from 'aws-amplify'
+import appSyncConfig from './AppSyncAccess'
+
+const amplifyConfig = {
+    'aws_appsync_graphqlEndpoint': appSyncConfig.graphqlEndpoint,
+    'aws_appsync_region': appSyncConfig.region,
+    'aws_appsync_authenticationType': appSyncConfig.authenticationType,
+    'aws_appsync_apiKey': appSyncConfig.apiKey
+}
+Amplify.configure(amplifyConfig)
 
 Vue.config.productionTip = false
 
