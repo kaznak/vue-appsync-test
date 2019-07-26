209ad0e2912fdbeeabc1969c71cabd48dc7e4549
kaznak <kaznak.at.work@gmail.com>
2019-07-26 14:22:58 +0900

    wip: user router-view

--- src/App.vue
+++ src/App.vue
@@ -16,23 +16,17 @@
     </v-app-bar>
 
     <v-content>
-      <home />
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
-import Home from './components/Home';
-
 export default {
   name: 'App',
   components: {
-//    HelloWorld,
-    Home,
   },
   data: () => ({
-    //
   }),
 };
 </script>
